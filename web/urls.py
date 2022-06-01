53eee71e5199440caeb2e9218203dd6ba53260d4
OriginalCyber <Cyber777378@gmail.com>
2022-06-01 19:55:25 +0700

    v4

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from web.views import index, urgent, low, medium
+from web.views import index, urgent, low, medium, show
 
 urlpatterns = [
-    path('', index),
+    path('index/', index, name='index'),
+    path('', show, name='show'),
     path('urgent/', urgent, name='urgent'),
     path('low/', low, name='low'),
-    path('medium/', medium, name='medium')
+    path('medium/', medium, name='medium'),
+
 ]
--- web/views.py
+++ web/views.py
@@ -5,10 +5,13 @@ def index(request):
     return render(request, 'index.html')
 
 def urgent(request):
-    return render(request,'urgent Page')
+    return render(request,'urgent.html')
 
 def low(request):
-    return render(request,'low Page')
+    return render(request,'low.html')
 
 def medium(request):
-    return render(request,'medium Page')
+    return render(request,'medium.html')
+
+def show(request):
+    return render(request,'show.html')
